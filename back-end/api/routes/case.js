ea619e73bb16c66d63b2bbb900752a15064f7994
Odaiba13 <89085584+Odaiba13@users.noreply.github.com>
2023-06-29 05:03:22 -0400

    Add files via upload

---
+++ back-end/api/routes/case.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const router = express.Router();
+const mysqlConnection = require('../connection/connection');
+const jwt = require('jsonwebtoken');
+
+router.get('/',(req,res)=> {
+    mysqlConnection.query('select * from cases',(err,rows,fields)=> {
+        if(!err){
+            res.json(rows);
+        }else{
+            console.log(err);
+        }
+    })
+});
+
+router.post('/InsertCase', (req,res) => {
+    mysqlConnection.query('insert into cases(caseName,dataCase,Noncoplanar) values(?,?,?)',
+    [req.body.caseName,req.body.dataCase,req.body.Noncoplanar],(err,respuesta) =>{
+        if(err){
+            console.log(err);
+        }else{
+            console.log(respuesta.rows);
+            res.send(JSON.stringify({"status":"ok","item":respuesta.rows}));
+        }
+    })
+    mysqlConnection.query('insert into typeofcancer(cancerName,cancerDescription) values(?,?)',
+    [req.body.cancerName,req.body.cancerDescription],(err,respuesta) =>{
+        if(err){
+            console.log(err);
+        }
+        // else{
+        //     console.log(respuesta.rows);
+        //     res.send(JSON.stringify({"status":"ok","item":respuesta.rows}));
+        // }
+    })
+})
+
+module.exports = router;
