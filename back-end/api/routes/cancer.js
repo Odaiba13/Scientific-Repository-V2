3eb2b4a78144287f18d9e24c488fd57948b3e0ed
Odaiba13 <89085584+Odaiba13@users.noreply.github.com>
2023-07-07 14:11:22 -0400

    Add files via upload

---
+++ back-end/api/routes/cancer.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const router = express.Router();
+const mysqlConnection = require('../connection/connection');
+const jwt = require('jsonwebtoken');
+
+router.get('/ListarCancers',(req,res)=> {
+    mysqlConnection.query('select * from typeofcancer',(err,rows,fields)=> {
+        if(!err){
+            res.json(rows);
+        }else{
+            console.log(err);
+        }
+    })
+});
+
+router.post('/InsertCancer', (req,res) => {
+    mysqlConnection.query('insert into typeofcancer(cancerName,cancerDescription) values(?,?)',
+    [req.body.cancerName,req.body.cancerDescription],(err,respuesta) =>{
+        if(err){
+            console.log(err);
+        }else{
+            console.log(respuesta.insertId);
+            res.status(201).send({message:"Cancer created Successfully"});
+        }
+    })
+})
+router.delete('/:id',(req,res)=>{
+    const id = req.params.id;
+    mysqlConnection.query(`delete from typeofcancer where idCancer=${id}`, (err,resp,fields)=>{
+        if(err){
+            console.log(err);
+        }else{
+            res.status(204).send({
+                message: "Cancer Deleted"
+            });
+        }
+    })
+})
+
+module.exports = router;
